d68dc41cc85ed5978ca7811b281869b42e1c6405
Lucas Viana <lcasviana@gmail.com>
2026-04-01 17:39:01 -0300

    core: chess index

---
+++ src/chess/index.ts
@@ -0,0 +1,4 @@
+export { Chess, SQUARES } from "./adapter";
+export type { ChessColor, ChessSquare as Square, ChessPieceSymbol as PieceSymbol, ChessPiece, ChessMove as Move } from "./adapter";
+export { ChessEngine } from "./chess";
+export { Piece, Color, MoveFlags } from "./types";
